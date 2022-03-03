eac16d85769279c177069b43600ed770ef725361
TFQ_Moon <no1msh1217@gmail.com>
2022-03-03 23:24:28 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kotlinintentindependentstudy3/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.kotlinintentindependentstudy3
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnDial.setOnClickListener {
+
+            val inputStr = edtPhoneNum.text.toString()
+            val myUrl = Uri.parse("tel:${inputStr}")
+            val dialIntent = Intent(Intent.ACTION_DIAL , myUrl)
+            startActivity(dialIntent)
+
+        }
+
+        btnSendTo.setOnClickListener {
+
+            val inputStr = edtPhoneNum.text.toString()
+            val myUri = Uri.parse("smsto:${inputStr}")
+            val sendToIntent = Intent(Intent.ACTION_SENDTO , myUri)
+            sendToIntent.putExtra("sms_body" , "안녕하세요.")
+            startActivity(sendToIntent)
+        }
+
+        btnView.setOnClickListener {
+
+            val myUri = Uri.parse("http://m.naver.com")
+            val viewIntent = Intent(Intent.ACTION_VIEW , myUri)
+            startActivity(viewIntent)
+        }
+
+        btnViewPlayStore.setOnClickListener {
+
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent(Intent.ACTION_VIEW , myUri)
+            startActivity(myIntent)
+        }
+    }
+}
